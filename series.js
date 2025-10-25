59bd8ca9dd169ef13e57a33f2f8d9b0e8c631f7c
sakthi s <sakthipandiyan826@gmail.com>
2025-10-25 13:12:19 +0530

    Add files via upload

--- series.js
+++ series.js
@@ -3,82 +3,84 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.default = parallel;
-
-var _eachOf = require('./eachOf.js');
-
-var _eachOf2 = _interopRequireDefault(_eachOf);
+exports.default = series;
 
 var _parallel2 = require('./internal/parallel.js');
 
 var _parallel3 = _interopRequireDefault(_parallel2);
 
+var _eachOfSeries = require('./eachOfSeries.js');
+
+var _eachOfSeries2 = _interopRequireDefault(_eachOfSeries);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /**
- * Run the `tasks` collection of functions in parallel, without waiting until
- * the previous function has completed. If any of the functions pass an error to
- * its callback, the main `callback` is immediately called with the value of the
- * error. Once the `tasks` have completed, the results are passed to the final
- * `callback` as an array.
- *
- * **Note:** `parallel` is about kicking-off I/O tasks in parallel, not about
- * parallel execution of code.  If your tasks do not use any timers or perform
- * any I/O, they will actually be executed in series.  Any synchronous setup
- * sections for each task will happen one after the other.  JavaScript remains
- * single-threaded.
- *
- * **Hint:** Use [`reflect`]{@link module:Utils.reflect} to continue the
- * execution of other tasks when a task fails.
+ * Run the functions in the `tasks` collection in series, each one running once
+ * the previous function has completed. If any functions in the series pass an
+ * error to its callback, no more functions are run, and `callback` is
+ * immediately called with the value of the error. Otherwise, `callback`
+ * receives an array of results when `tasks` have completed.
  *
  * It is also possible to use an object instead of an array. Each property will
- * be run as a function and the results will be passed to the final `callback`
+ * be run as a function, and the results will be passed to the final `callback`
  * as an object instead of an array. This can be a more readable way of handling
- * results from {@link async.parallel}.
+ *  results from {@link async.series}.
  *
- * @name parallel
+ * **Note** that while many implementations preserve the order of object
+ * properties, the [ECMAScript Language Specification](http://www.ecma-international.org/ecma-262/5.1/#sec-8.6)
+ * explicitly states that
+ *
+ * > The mechanics and order of enumerating the properties is not specified.
+ *
+ * So if you rely on the order in which your series of functions are executed,
+ * and want this to work on all platforms, consider using an array.
+ *
+ * @name series
  * @static
  * @memberOf module:ControlFlow
  * @method
  * @category Control Flow
- * @param {Array|Iterable|AsyncIterable|Object} tasks - A collection of
- * [async functions]{@link AsyncFunction} to run.
- * Each async function can complete with any number of optional `result` values.
+ * @param {Array|Iterable|AsyncIterable|Object} tasks - A collection containing
+ * [async functions]{@link AsyncFunction} to run in series.
+ * Each function can complete with any number of optional `result` values.
  * @param {Function} [callback] - An optional callback to run once all the
- * functions have completed successfully. This function gets a results array
- * (or object) containing all the result arguments passed to the task callbacks.
- * Invoked with (err, results).
- * @returns {Promise} a promise, if a callback is not passed
- *
+ * functions have completed. This function gets a results array (or object)
+ * containing all the result arguments passed to the `task` callbacks. Invoked
+ * with (err, result).
+ * @return {Promise} a promise, if no callback is passed
  * @example
  *
  * //Using Callbacks
- * async.parallel([
+ * async.series([
  *     function(callback) {
  *         setTimeout(function() {
+ *             // do some async task
  *             callback(null, 'one');
  *         }, 200);
  *     },
  *     function(callback) {
  *         setTimeout(function() {
+ *             // then do another async task
  *             callback(null, 'two');
  *         }, 100);
  *     }
  * ], function(err, results) {
  *     console.log(results);
- *     // results is equal to ['one','two'] even though
- *     // the second function had a shorter timeout.
+ *     // results is equal to ['one','two']
  * });
  *
- * // an example using an object instead of an array
- * async.parallel({
+ * // an example using objects instead of arrays
+ * async.series({
  *     one: function(callback) {
  *         setTimeout(function() {
+ *             // do some async task
  *             callback(null, 1);
  *         }, 200);
  *     },
  *     two: function(callback) {
  *         setTimeout(function() {
+ *             // then do another async task
  *             callback(null, 2);
  *         }, 100);
  *     }
@@ -88,7 +90,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  * });
  *
  * //Using Promises
- * async.parallel([
+ * async.series([
  *     function(callback) {
  *         setTimeout(function() {
  *             callback(null, 'one');
@@ -101,21 +103,22 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  *     }
  * ]).then(results => {
  *     console.log(results);
- *     // results is equal to ['one','two'] even though
- *     // the second function had a shorter timeout.
+ *     // results is equal to ['one','two']
  * }).catch(err => {
  *     console.log(err);
  * });
  *
  * // an example using an object instead of an array
- * async.parallel({
+ * async.series({
  *     one: function(callback) {
  *         setTimeout(function() {
+ *             // do some async task
  *             callback(null, 1);
  *         }, 200);
  *     },
  *     two: function(callback) {
  *         setTimeout(function() {
+ *             // then do another async task
  *             callback(null, 2);
  *         }, 100);
  *     }
@@ -129,21 +132,22 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  * //Using async/await
  * async () => {
  *     try {
- *         let results = await async.parallel([
+ *         let results = await async.series([
  *             function(callback) {
  *                 setTimeout(function() {
+ *                     // do some async task
  *                     callback(null, 'one');
  *                 }, 200);
  *             },
  *             function(callback) {
  *                 setTimeout(function() {
+ *                     // then do another async task
  *                     callback(null, 'two');
  *                 }, 100);
  *             }
  *         ]);
  *         console.log(results);
- *         // results is equal to ['one','two'] even though
- *         // the second function had a shorter timeout.
+ *         // results is equal to ['one','two']
  *     }
  *     catch (err) {
  *         console.log(err);
@@ -156,11 +160,13 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  *         let results = await async.parallel({
  *             one: function(callback) {
  *                 setTimeout(function() {
+ *                     // do some async task
  *                     callback(null, 1);
  *                 }, 200);
  *             },
  *            two: function(callback) {
  *                 setTimeout(function() {
+ *                     // then do another async task
  *                     callback(null, 2);
  *                 }, 100);
  *            }
@@ -174,7 +180,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  * }
  *
  */
-function parallel(tasks, callback) {
-    return (0, _parallel3.default)(_eachOf2.default, tasks, callback);
+function series(tasks, callback) {
+    return (0, _parallel3.default)(_eachOfSeries2.default, tasks, callback);
 }
 module.exports = exports.default;
